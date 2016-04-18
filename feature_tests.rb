99d149d32278ae6e3f897eac937b2c3c1be518cf
pixelandpage <mags@pixelandpage.co.uk>
2016-04-18 19:48:41 +0100

    added initialize oystercard with balance 0

---
+++ feature_tests.rb
@@ -0,0 +1,7 @@
+require './lib/oystercard'
+
+card = Oystercard.new
+puts card
+# In order to use public transport
+# As a customer
+# I want money on my card
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,30 @@
+require 'oystercard'
+
 describe Oystercard do
+subject (:card) {described_class.new}
+
+  describe '#initialize' do
+
+    it 'should have a balance of 0' do
+      expect(card).to respond_to(:balance).with 0
+    end
+
+    # it 'should initlaize with a default balance' do
+    #   expect(subject.balance?).to eq(Oysetcard::DEFAULT_BALANCE)
+    # end
+
+  end
+
+  # describe '#top_up' do
+  #
+  #   it{should respond_to(:top_up)}
+  #
+  #   it 'should be able to be topped up'
+  #     expect(subject.top_up).to change(balance)
+  #   end
+  #
+  #
+  # end
+
 
 end
